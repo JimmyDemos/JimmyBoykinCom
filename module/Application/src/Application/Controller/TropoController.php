fb16ca480fe9f19a4ca37d7b7a116ea3deaa3dc5
Jimmy <jimmy@flazzle.com>
2016-03-24 20:54:36 -0400

    first set of demos working

--- module/Application/src/Application/Controller/TropoController.php
+++ module/Application/src/Application/Controller/TropoController.php
@@ -23,17 +23,12 @@ class TropoController extends AbstractActionController
      */
     public function indexAction()
     {
-        $config = $this->getServiceLocator()->get('config');
-        $token = $config['tropotextkey'];
-        $tropo = new Tropo();//to get sessionapi to autoload hack
-        $session = new SessionAPI();
-        $sessionid = $session->createSession($token);
-        
-        $this->pushText($sessionid, '19199438138', 'Whoop!');
-        
-        $viewArray = array('sid'=>$sessionid);
+        $demoid = rand(1,99999);
+
+        $viewArray = array('demoid'=>$demoid);
         
         return $viewArray;
+        
     }
     
     public function textAction()
@@ -46,7 +41,8 @@ class TropoController extends AbstractActionController
         } else {
             //echo("No session provided!");
         }
-        
+
+        /** If message is outgoing **/
         if(!empty($post['session']['parameters']['action']) && $post['session']['parameters']['action'] == 'create'){
             $text = $this->popText($sessionid);
             $tropo = new Tropo();
@@ -56,9 +52,65 @@ class TropoController extends AbstractActionController
             $model = new JsonModel($result);
             return $model;
         }
-        
-        
 
+        /** Message is incoming **/
+        $phone = $post['session']['from']['id'];
+        $content = $post['session']['initialText'];
+        
+        if(is_numeric($content)){
+            $demoid = $content;
+            //demoid verification
+            $this->pushDemo($demoid, $phone, 'Guest');
+            die('ok authed');
+        } elseif(strcasecmp('yes',$content) == 0) {
+            $demoid = $this->getDemoIDByPhone($phone);
+            $this->pushDemo($demoid, $phone, 'Guest', 'Safe!');
+            die('ok safe');
+        } elseif(strcasecmp('no',$content) == 0) {
+            $demoid = $this->getDemoIDByPhone($phone);
+            $this->pushDemo($demoid, $phone, 'Guest', 'Help!');
+            die('ok help');
+        }
+        
+    }
+
+    
+    public function demoDataAction()
+    {
+        $demoid = $this->params('param');
+        $demodata = $this->getDemo($demoid);
+        $model = new JsonModel($demodata);
+        return $model;
+    }
+    
+    public function alertAction()
+    {
+        $demoid = $this->params('param');
+        $demodata = $this->getDemo($demoid);    
+        $phone = $demodata['phone'];
+        
+        $config = @$this->getServiceLocator()->get('config');
+        $token = $config['tropotextkey'];
+        $tropo = new Tropo();//to get sessionapi to autoload hack
+        $session = new SessionAPI();
+        $sessionid = $session->createSession($token);
+        $this->pushText($sessionid, $phone, 'THIS IS A TEST ALERT!');
+        die('ok');
+    }
+    
+    public function drillAction()
+    {
+        $demoid = $this->params('param');
+        $demodata = $this->getDemo($demoid);
+        $phone = $demodata['phone'];
+        
+        $config = @$this->getServiceLocator()->get('config');
+        $token = $config['tropotextkey'];
+        $tropo = new Tropo();//to get sessionapi to autoload hack
+        $session = new SessionAPI();
+        $sessionid = $session->createSession($token);
+        $this->pushText($sessionid, $phone, 'THIS IS A TEST EMERGENCY DRILL! Are you safe? Please respond YES or NO');
+        die('ok');  
     }
     
     public function voiceAction()
@@ -93,5 +145,46 @@ class TropoController extends AbstractActionController
         $textQueue = serialize($textQueue);
         file_put_contents('textqueue.txt', $textQueue);
     }
+    
+    
+    private function getDemo($demoid)
+    {
+        $file = file_get_contents('demo.txt');
+        $demo = unserialize($file);
+        if(!empty($demo[$demoid]))
+            return $demo[$demoid];
+        return array();
+    }
+    
+    private function getDemoIDByPhone($phone)
+    {
+        $file = file_get_contents('demo.txt');
+        $demo = unserialize($file);
+        
+        foreach($demo as $key => $needle){
+            if($needle['phone'] == $phone)
+                return $key;
+        }
+       return false;
+    }
+    
+    private function pushDemo($demoid, $phone, $name, $status = '')
+    {
+        $file = file_get_contents('demo.txt');
+        $demo = unserialize($file);
+        
+        //Delete any other demos by this number
+        $oldDemoKey = $this->getDemoIDByPhone($phone);
+        if($oldDemoKey)
+            unset($demo[$oldDemoKey]);
+
+        
+        $demo[$demoid]['phone'] = $phone;
+        $demo[$demoid]['name'] = $name;
+        $demo[$demoid]['status'] = $status;
+        $demo = serialize($demo);
+        file_put_contents('demo.txt', $demo);
+    }
+    
     
 }
